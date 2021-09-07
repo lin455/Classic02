01e038d6e0a103ef5512009e5740bc24bca2eeec
lin455 <jack08281113@gmail.com>
2021-09-07 21:24:53 +0800

    Initial Commit

---
+++ Classic03/Classic03App.swift
@@ -0,0 +1,17 @@
+//
+//  Classic03App.swift
+//  Classic03
+//
+//  Created by 暨大附中10 on 2021/9/7.
+//
+
+import SwiftUI
+
+@main
+struct Classic03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
